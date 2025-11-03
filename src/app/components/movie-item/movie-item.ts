1f7bd1fe3e24a90cd076afc8e6471614b11ec953
Virginia Garcia <85966206+virginia-garcia@users.noreply.github.com>
2025-11-03 08:16:43 -0300

    Update movie-item.ts

--- src/app/components/movie-item/movie-item.ts
+++ src/app/components/movie-item/movie-item.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Movie } from '../../interfaces/movie';
 
 @Component({
   selector: 'app-movie-item',
-  imports: [],
+  standalone: true,
+  imports: [NgIf],
   templateUrl: './movie-item.html',
-  styleUrl: './movie-item.css',
+  styleUrl: './movie-item.css'
 })
-export class MovieItem {
+export class MovieItemComponent {
+  @Input() movie!: Movie;
+  @Output() movieSelected = new EventEmitter<string>();
+  @Output() trailerRequested = new EventEmitter<string>();
 
+  selectMovie(): void {
+    this.movieSelected.emit(this.movie.title);
+  }
+
+  openTrailer(event: Event): void {
+    event.stopPropagation();
+    if (this.movie.trailerUrl) {
+      this.trailerRequested.emit(this.movie.trailerUrl);
+    }
+  }
 }
