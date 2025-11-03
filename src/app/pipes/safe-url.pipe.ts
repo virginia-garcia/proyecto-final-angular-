1c2bf2f8be96091df19972c34e6be18e1e07e074
Virginia Garcia <85966206+virginia-garcia@users.noreply.github.com>
2025-11-03 08:26:37 -0300

    Rename SafeUrlPipe to SafePipe

--- src/app/pipes/safe-url.pipe.ts
+++ src/app/pipes/safe-url.pipe.ts
@@ -2,12 +2,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Pipe({
-  name: 'safeUrl',
-  standalone: true 
+  name: 'safe',
+  standalone: true
 })
-export class SafeUrlPipe implements PipeTransform {
+export class SafePipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
-  
+
   transform(url: string): SafeResourceUrl {
     return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
